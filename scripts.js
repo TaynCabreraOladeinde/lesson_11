6a2e2fa76b6bacccdfb857e5587ee26a45dd19d9
Tayn Cabrera <tayn.olad@gmail.com>
2025-04-06 12:45:07 -0700

    added comments(Why is the date still not showing up on display)

--- scripts.js
+++ scripts.js
@@ -16,13 +16,16 @@ function checkTime(i) {
     return i;
 }
 
+// Date display
 var sp = "/";
 var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth() + 1; // January is 0
 var yyyy = today.getFullYear();
 
-if (dd < 10) dd = '0' + dd;
-if (mm < 10) mm = '0' + mm;
+// Add leading zeros for single digits
+if(dd < 10) dd = '0' + dd;
+if(mm < 10) mm = '0' + mm;
 
+// Display the date using jQuery
 $("#date").html(mm + sp + dd + sp + yyyy);

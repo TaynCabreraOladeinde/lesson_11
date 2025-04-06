09aa20c9a652eeb4366ff57974e3cb3bfbeff2b2
Tayn Cabrera <tayn.olad@gmail.com>
2025-04-06 12:38:18 -0700

    data not showing up previously (bug fix)

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function startTime() {
     let s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
     setTimeout(startTime, 1000);
 }
 
@@ -15,13 +15,15 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-function checkDate(i) {
-    var today = new Date();
-    console.log(today);
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    $("#date").html((year + "/" + month + "/" + day));
-}
 
+/* --- FUNCTION FOR THE DATE --- */
+var sp = "/";
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; // January is 0
+var yyyy = today.getFullYear();
 
+if (dd < 10) dd = '0' + dd;
+if (mm < 10) mm = '0' + mm;
+
+$("#date").html(mm + sp + dd + sp + yyyy);
